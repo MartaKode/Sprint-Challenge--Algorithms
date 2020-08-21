db4efa8db69a2f6edf5672e1bbed1e21adc61672
Marta Krawczyk <martaalicja113@gmail.com>
2020-08-21 13:47:43 -0400

    Sprint completed, no stretch

--- Short-Answer/Algorithms_Answers.md
+++ Short-Answer/Algorithms_Answers.md
@@ -2,14 +2,90 @@
 
 ## Exercise I
 
-a)
+a)  Linear O(n)
+rewriting the while loop to look a little cleaner , we have:
+```python
+    while(a < n**3):
+        a += n**2
+```
+n^3 contains exactly n number of n^2's --> 
+```python
+3**3 = 27 
+3**2 = 9 
+9 + 9 + 9 = 27 # -> n is 3 -> n * n**2 = n**3  or n**3/n**2 = n
+```
+n^3 / n ^2 == n therefore there will be n operations performend no matter how big n is; therefore , we have a linear time complexity O(n)
 
+b) O(n log n)
+```python
+sum = 0  #O(1)
+    for i in range(n): #O(n)
+      j = 1 # O(1)
+      while j < n: # O(log n) because j increases by power of 2, therefore approaches n twice as quickly
+        j *= 2
+        sum += 1 # O(1)
+```
+Looking at it from an example perspective:
+n = 2
+number of iterations should be 1, because 2^1 <= 2 or log 2 = 1
+n= 3
+number of iterations should be between 1 and 2, because 2^1 < 3 < 2^2
+n= 5
+number of iterations should be between 2 and 3, because 2^2 < 5 < 2^3 
+n=100
+number of iterations should be between 6 and 7, because 2^6 < 100 <7
 
-b)
+Combining all of that together(we multiply everything inside the foorloop)
+O(1 + n(1 + (logn *1))) -> drop constants -> O(n log n)
 
+c) Linear O(n)
+```python
+def bunnyEars(bunnies): 
+      if bunnies == 0: # O(1)
+        return 0 
 
-c)
+      return 2 + bunnyEars(bunnies-1) # O(n)
+```
+we're dealing with a simple recursion call, which in most cases will have a linear time complexity O(n).
+For each n we put in, our output is 2 times it since each bunny has 2 ears, so we can say that the run time would be O(2n) --> drop constants --> O(n)
 
 ## Exercise II
 
+This problem is very similar to guessing a number that the computer picked that was described during one of our lectures. 
+Similarly, we could use binary search:
 
+```python
+# think of the number of floors as a sorted array of ascending numbers
+# our `first failure floor` f is somewhere in that array
+
+# firt of all, check if the egg breaks on the very first floor, because if it does, we're done: just return that first floor
+
+# define a start point 
+# define an end point
+# find the middle of given array  => (start + end)/ 2
+
+# if we want to use recursion, we could think of the stopping point (base case):
+    # probably when our start point becomes smaller or equal to our end point, meaning we cant halve/split the array anymore
+
+# check if the egg breaks on the middle floor, if so:
+    # we could potentially check one floor below the middle, if the egg doesnt break there: we're done! the middle is our f(the first floor to start breaking eggs), otherwise: 
+    # we must look lower
+    # our start point stays the same
+    # our endpoint is now middle - 1(since middle was already checked)
+    # we can use recursion to start over the process with the left half of our array and overwrite old middle with a new one
+
+# otherwise, the egg doesnt break
+    # for sanity check we check just a floor above it and see if the egg breaks, if so: we're done! the middle is our f(the first floor to start breaking eggs), otherwise: 
+    # we must look higher
+    # our start point becomes middle +1 (since we already checked the middle)
+    # our endpoint stays the same
+    # we can use recursion to start over the process with the right half of our array and overwrite old middle with a new one
+
+# the process would continue until we'd find the first floor to break eggs
+
+# in case none of the floors are breaking eggs, we could just return None at the end 
+
+# the runtime of this process would be logarythmic O(log n) since we're halving the array/number of things we're considering each time 
+
+
+```
--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -97,16 +97,147 @@ class SortingRobot:
         Sort the robot's list.
         """
         # Fill this out
-        pass
+        # variables not allowed :( 
+        # pass
 
+        # insertion sort?
+        # while the left neighbor> right neighbor, swap, so the smallest values stays to the left
+
+        # we are at 0, we want to start at second element so lets move right
+        # if Im not holding anything(light is off)-->im holding None AND can move to the right: go right
+        while self.can_move_right() and self.light_is_on() is False:
+            # move right
+            self.move_right()
+            # pick up our item (swap with None)
+            self.swap_item() 
+            self.set_light_on() # Holding item
+            # in place of our item there is NONE 
+
+            # while we can move left AND our item is smaller than the left neighbor: swap
+
+            # if we are holding an item and can go left
+            while self.can_move_left() and self.light_is_on() is True:
+                # move left 
+                self.move_left()
+                # and check if our item is smaller than the left neighbor
+                if self.compare_item() == -1:
+                    # swap them: put our item down and hold the bigger one
+                    self.swap_item() # since we're not holding None, the light stays on
+
+                    # now were holding the bigger item
+                    # go back right where None is,
+                    self.move_right()
+                    # put item down and hold None 
+                    self.swap_item()
+                    self.set_light_off()
+                    # then come back (left) with None
+                    self.move_left()
+
+                    #pick the bigger item back up again --> put None down
+                    self.swap_item()
+                    self.set_light_on() # now we're holding the bigger item and can continue on
+                    
+                # otherwise, our item is bigger that its left neighbor:
+                # we want to go back to our place(right) and put our item down
+                else:
+                    self.move_right()
+                    # drop item 
+                    self.swap_item() # -> pick None back up
+                    self.set_light_off() # not holding any item --> Im holding None
+
+            # If I am all the way to the left(cant move left anymore), drop my item
+            if self.can_move_left() is False:
+                # drop item
+                self.swap_item() # pick up None
+                self.set_light_off() # holding NONE
+ 
 
 if __name__ == "__main__":
     # Test our your implementation from the command line
     # with `python robot_sort.py`
 
     l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
-
+    # l = [2,4,5,3,1,0]
+    # l = [2,1]
     robot = SortingRobot(l)
 
     robot.sort()
     print(robot._list)
+
+
+
+    # ```🍝 Spaghetti code 🍝```
+
+        # selection sort:
+        # put biggest number at the end (swap with whatever is at the end currently) 
+        # once we have the biggest number, we can compare the last values
+        # loop through the list 
+            # looping might mean moving left and right?
+            # find our max value
+        ##----> we are at zero, 
+        ## pick up the item and turn on the light (thats our current "max" value)
+        # check if we can move right
+        # while we can move right
+            # compare each item
+            # if the new item is bigger, swap them, and pick up the new item instea
+        # pick it up, turn on the robot ligh
+        # swap it with the last index
+        # turn off light
+        # recurse on list without the last value?
+        
+
+### maybe bubble sort? Recursion?
+        # compare 2 values at a time and swap if the left value > right value
+        # recursion? 
+
+        # base case?
+        # if we can't move right
+            
+        # # start at 0 --> already there!
+        # # pick up the item 
+        # self.swap_item()
+        # # turn on ligt 
+        # self.set_light_on()
+        # # move to the right, now we're at position 1
+        # self.move_right()
+
+
+        # # check if we can move right
+        # # while we can move right, we can perform certain steps:
+        # while self.can_move_right() and self.light_is_on():
+        #     # compare the items:
+        #     # if our item > item on the floor, swap
+        #     if self.compare_item() == 1:
+        #         # swap
+        #         self.swap_item()
+        #         # turn the light off
+        #         self.set_light_off()
+        #         # pick up the new item and wait
+        #         self.swap_item()
+        #         self.set_light_on()
+        #     # if our item < item on the floor
+        #     if self.compare_item() == -1:
+        #         # go back left, 
+        #         self.move_left()
+        #         # drop item
+        #         self.swap_item()
+        #         # turn light off
+        #         self.set_light_off()
+        #         # go right
+        #         self.move_right()
+        #         # pick up item
+        #         self.swap_item()
+
+        #     # move to the right and repeat
+            # self.move_right()
+
+        # self.swap_item()
+        # now we're holding our second biggest number and are at the very end of list
+        # # check if can move left
+        # while self.can_move_left():
+        # # move all the way to the beginning
+        #     self.move_left()
+
+        # # start over
+        
+    
